67bb3fc18ec5404083bd91d45135703437229f28
Nathan Tapia-Ramirez <ntapiaramirez@outlook.com>
2024-05-16 14:04:22 -0400

    updated robotic pet

--- classes/RoboticPet.php
+++ classes/RoboticPet.php
@@ -1,10 +1,12 @@
 <?php
 
-class RoboticPet {
+class RoboticPet extends pet {
     private $_accessories = [];
 
-    public function __construct($_accessories)
+    public function __construct($_animal="unknown",$_color="unknown",$_accessories)
     {
+        parent::__construct($_animal,$_color);
+
         $this->_accessories = $_accessories;
     }
 }
