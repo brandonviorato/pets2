2835761879e6922fd2f99880230299b2581540bd
brandonviorato <brandonviorato@outlook.com>
2024-05-16 13:43:20 -0400

    Created pet class

---
+++ classes/pet.php
@@ -0,0 +1,13 @@
+<?php
+
+class Pet
+{
+    private $_animal;
+    private $_color;
+
+    public function __construct($_animal="", $_color="")
+    {
+        $this->_animal = $_animal;
+        $this->_color = $_color;
+    }
+}
